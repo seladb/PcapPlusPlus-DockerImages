a9ae825caddf0c3954cdb07063ec79e368d41cdc
seladb <seladb@yahoo.com>
2017-09-19 14:48:32 +0100

    Added centos7 docker build

---
+++ build-centos7.sh
@@ -0,0 +1,2 @@
+sudo docker build -f Dockerfile-centos7 -t centos7 .
+sudo docker tag centos7 seladb/centos7
---
+++ run-centos7.sh
@@ -0,0 +1,7 @@
+git clone http://github.com/seladb/PcapPlusPlus.git
+sudo docker pull seladb/centos7
+sudo docker run -itd --name centos7 -v $(pwd)/PcapPlusPlus:/PcapPlusPlus seladb/centos7 bash
+sudo docker exec -i centos7 bash -c "cd PcapPlusPlus && ./configure-linux.sh --default"
+sudo docker exec -i centos7 bash -c "cd PcapPlusPlus && make all"
+sudo docker exec -i centos7 bash -c "cd PcapPlusPlus/Tests/Packet++Test && Bin/Packet++Test"
+sudo docker exec -i centos7 bash -c "cd PcapPlusPlus/Tests/Pcap++Test && Bin/Pcap++Test -n"
