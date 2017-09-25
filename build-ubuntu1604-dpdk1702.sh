32943de3a55ef533b2d2ddea73c3e37bf5267528
seladb <seladb@yahoo.com>
2017-09-25 15:43:42 +0100

    Added DPDK 17.02 image

---
+++ build-ubuntu1604-dpdk1702.sh
@@ -0,0 +1,2 @@
+sudo docker build -f Dockerfile-ubuntu1604-dpdk1702 -t ubuntu1604-dpdk1702 .
+sudo docker tag ubuntu1604-dpdk1702 seladb/ubuntu1604-dpdk1702
---
+++ run-ubuntu1604-dpdk1702.sh
@@ -0,0 +1,8 @@
+git clone http://github.com/seladb/PcapPlusPlus.git
+sudo docker pull seladb/ubuntu1604-dpdk1702
+sudo docker run -itd --name ubuntu1604-dpdk1702 -v $(pwd)/PcapPlusPlus:/PcapPlusPlus seladb/ubuntu1604-dpdk1702 bash
+sudo docker exec -i ubuntu1604-dpdk1702 bash -c "cd PcapPlusPlus && ./configure-linux.sh --dpdk --dpdk-home /dpdk-17.02"
+sudo docker exec -i ubuntu1604-dpdk1702 bash -c "cd PcapPlusPlus && make all"
+sudo docker exec -i ubuntu1604-dpdk1702 bash -c "cd PcapPlusPlus/Tests/Packet++Test && Bin/Packet++Test"
+sudo docker exec -i ubuntu1604-dpdk1702 bash -c "cd PcapPlusPlus/Tests/Pcap++Test && Bin/Pcap++Test -n -k 0"
+
