456ae815d9ad7ed569351d8a9bdc0b44d2e398be
seladb <seladb@yahoo.com>
2017-09-25 13:27:34 +0100

    Added Ubuntu 14.04

---
+++ build-ubuntu1404.sh
@@ -0,0 +1,2 @@
+sudo docker build -f Dockerfile-ubuntu1404 -t ubuntu1404 .
+sudo docker tag ubuntu1404 seladb/ubuntu1404
---
+++ run-ubuntu1404.sh
@@ -0,0 +1,8 @@
+git clone http://github.com/seladb/PcapPlusPlus.git
+sudo docker pull seladb/ubuntu1404
+sudo docker run -itd --name ubuntu1404 -v $(pwd)/PcapPlusPlus:/PcapPlusPlus seladb/ubuntu1404 bash
+sudo docker exec -i ubuntu1404 bash -c "cd PcapPlusPlus && ./configure-linux.sh --default"
+sudo docker exec -i ubuntu1404 bash -c "cd PcapPlusPlus && make all"
+sudo docker exec -i ubuntu1404 bash -c "cd PcapPlusPlus/Tests/Packet++Test && Bin/Packet++Test"
+sudo docker exec -i ubuntu1404 bash -c "cd PcapPlusPlus/Tests/Pcap++Test && Bin/Pcap++Test -n"
+
