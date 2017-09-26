a36106598b68ecfad9c92177060c4dc2534fc474
seladb <seladb@yahoo.com>
2017-09-26 15:41:37 +0100

    Added Ubuntu 16.04 with PF_RING

---
+++ build-ubuntu1604-pfring.sh
@@ -0,0 +1,2 @@
+sudo docker build -f Dockerfile-ubuntu1604-pfring -t ubuntu1604-pfring .
+sudo docker tag ubuntu1604-pfring seladb/ubuntu1604-pfring
---
+++ run-ubuntu1604-pfring.sh
@@ -0,0 +1,8 @@
+git clone http://github.com/seladb/PcapPlusPlus.git
+sudo docker pull seladb/ubuntu1604-pfring
+sudo docker run -itd --name ubuntu1604-pfring -v $(pwd)/PcapPlusPlus:/PcapPlusPlus seladb/ubuntu1604-pfring bash
+sudo docker exec -i ubuntu1604-pfring bash -c "cd PcapPlusPlus && ./configure-linux.sh --pf-ring --pf-ring-home /PF_RING"
+sudo docker exec -i ubuntu1604-pfring bash -c "cd PcapPlusPlus && make all"
+sudo docker exec -i ubuntu1604-pfring bash -c "cd PcapPlusPlus/Tests/Packet++Test && Bin/Packet++Test"
+sudo docker exec -i ubuntu1604-pfring bash -c "cd PcapPlusPlus/Tests/Pcap++Test && Bin/Pcap++Test -n"
+
