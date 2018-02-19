87701fdb10973d2a993ba41c01a3de8c2d1fa616
seladb <seladb@yahoo.com>
2018-02-19 03:01:12 -0500

    Adding DPDK 17.11 image

---
+++ build-ubuntu1604-dpdk1711.sh
@@ -0,0 +1,2 @@
+sudo docker build -f Dockerfile-ubuntu1604-dpdk1711 -t ubuntu1604-dpdk1711 .
+sudo docker tag ubuntu1604-dpdk1711 seladb/ubuntu1604-dpdk1711
---
+++ run-ubuntu1604-dpdk1711.sh
@@ -0,0 +1,9 @@
+git clone http://github.com/seladb/PcapPlusPlus.git
+sudo docker pull seladb/ubuntu1604-dpdk1711
+sudo docker run -itd --name ubuntu1604-dpdk1711 -v $(pwd)/PcapPlusPlus:/PcapPlusPlus seladb/ubuntu1604-dpdk1711 bash
+sudo docker exec -i ubuntu1604-dpdk1711 bash -c "cd PcapPlusPlus && ./configure-linux.sh --dpdk --dpdk-home /dpdk-17.11"
+sudo docker exec -i ubuntu1604-dpdk1711 bash -c "cd PcapPlusPlus && make all"
+sudo docker exec -i ubuntu1604-dpdk1711 bash -c "cd PcapPlusPlus/Tests/Packet++Test && Bin/Packet++Test"
+sudo docker exec -i ubuntu1604-dpdk1711 bash -c "cd PcapPlusPlus/Tests/Pcap++Test && Bin/Pcap++Test -n -k 0"
+sudo docker exec -i ubuntu1604-dpdk1711 bash -c "cd PcapPlusPlus && make install"
+sudo docker exec -i ubuntu1604-dpdk1711 bash -c "cd PcapPlusPlus/Examples/Tutorials/Tutorial-HelloWorld && make -f Makefile.non_windows all && ./Tutorial-HelloWorld"
