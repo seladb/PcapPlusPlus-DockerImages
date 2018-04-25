b5eea397490980ca633b4a21b5f4ea2799dbfee1
seladb <seladb@yahoo.com>
2018-04-25 11:16:00 -0400

    Updated images for DPDK 16.11 and 17.11: version update + added pcap PMD

--- run-ubuntu1604-dpdk1711.sh
+++ run-ubuntu1604-dpdk1711.sh
@@ -1,7 +1,7 @@
 git clone http://github.com/seladb/PcapPlusPlus.git
 sudo docker pull seladb/ubuntu1604-dpdk1711
 sudo docker run -itd --name ubuntu1604-dpdk1711 -v $(pwd)/PcapPlusPlus:/PcapPlusPlus seladb/ubuntu1604-dpdk1711 bash
-sudo docker exec -i ubuntu1604-dpdk1711 bash -c "cd PcapPlusPlus && ./configure-linux.sh --dpdk --dpdk-home /dpdk-17.11"
+sudo docker exec -i ubuntu1604-dpdk1711 bash -c "cd PcapPlusPlus && ./configure-linux.sh --dpdk --dpdk-home /dpdk-stable-17.11.2"
 sudo docker exec -i ubuntu1604-dpdk1711 bash -c "cd PcapPlusPlus && make all"
 sudo docker exec -i ubuntu1604-dpdk1711 bash -c "cd PcapPlusPlus/Tests/Packet++Test && Bin/Packet++Test"
 sudo docker exec -i ubuntu1604-dpdk1711 bash -c "cd PcapPlusPlus/Tests/Pcap++Test && Bin/Pcap++Test -n -k 0"
