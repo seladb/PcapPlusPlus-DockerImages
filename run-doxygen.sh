a737bdcc3c6dcdf56ef2b9e452d6de3d8dcc98d6
seladb <pcapplusplus@gmail.com>
2020-03-10 04:50:57 -0400

    Fix doxygen run script

--- run-doxygen.sh
+++ run-doxygen.sh
@@ -1,9 +1,9 @@
 git clone http://github.com/seladb/PcapPlusPlus.git
 git clone https://github.com/PcapPlusPlus/pcapplusplus.github.io.git
 sudo docker pull seladb/doxygen
+cd PcapPlusPlus
+./configure-linux.sh --default
+make all
+cd ..
 sudo docker run -itd --name doxygen -v $(pwd)/PcapPlusPlus:/PcapPlusPlus -v $(pwd)/pcapplusplus.github.io:/pcapplusplus.github.io seladb/doxygen bash
-sudo docker exec -i doxygen bash -c "cd PcapPlusPlus && ./configure-linux.sh --default"
-sudo docker exec -i doxygen bash -c "cd PcapPlusPlus && make all"
-sudo docker exec -i doxygen bash -c "cd pcapplusplus.github.io/api-docs/doxygen && sed -i 's|D:\\Documents\\Elad\\PcapPlusPlus\\Dist|/PcapPlusPlus/Dist|g' Doxyfile"
-sudo docker exec -i doxygen bash -c "cd pcapplusplus.github.io/api-docs/doxygen && doxygen Doxyfile"
-sudo docker exec -i doxygen bash -c "cd pcapplusplus.github.io/api-docs/html && ls -la"
+sudo docker exec -i doxygen bash -c "cd /pcapplusplus.github.io/api-docs/doxygen && doxygen Doxyfile-ci"
