ce9ccc18b02c060e1fb79dabc071fb2d1d06286d
seladb <pcapplusplus@gmail.com>
2021-09-21 07:58:30 +0530

    - Modify and enhance the script - Delete the rest of the scripts

--- run-ubuntu1604.sh
+++
@@ -1,9 +0,0 @@
-git clone http://github.com/seladb/PcapPlusPlus.git
-sudo docker pull seladb/ubuntu1604
-sudo docker run -itd --name ubuntu1604 -v $(pwd)/PcapPlusPlus:/PcapPlusPlus seladb/ubuntu1604 bash
-sudo docker exec -i ubuntu1604 bash -c "cd PcapPlusPlus && ./configure-linux.sh --default"
-sudo docker exec -i ubuntu1604 bash -c "cd PcapPlusPlus && make all"
-sudo docker exec -i ubuntu1604 bash -c "cd PcapPlusPlus/Tests/Packet++Test && Bin/Packet++Test"
-sudo docker exec -i ubuntu1604 bash -c "cd PcapPlusPlus/Tests/Pcap++Test && Bin/Pcap++Test -n"
-sudo docker exec -i ubuntu1604 bash -c "cd PcapPlusPlus && make install"
-sudo docker exec -i ubuntu1604 bash -c "cd PcapPlusPlus/Examples/Tutorials/Tutorial-HelloWorld && make -f Makefile.non_windows all && ./Tutorial-HelloWorld"
--- test-pcapplusplus.sh
+++ test-pcapplusplus.sh
@@ -1,7 +1,18 @@
+# Usage:
+# ------
+# ./test-pcapplusplus.sh IMAGE [OPTIONAL_TEST_PARAMS]
+#
+# For example:
+# ./test-pcapplusplus.sh ubuntu2004-dpdk1911
+# ./test-pcapplusplus.sh centos7 -s
+
+
 # exit when any command fails
 set -e
 
+# This part runs only inside the container - the script runs itelsf inside the container
 if [ "$1" == "build_and_run" ]; then
+    # Find the required configuration - default ,DPDK or PF_RING
     CONFIG_PARAMS="--default"
     if [ "$2" == "dpdk" ]; then
         CONFIG_PARAMS="--dpdk --dpdk-home /dpdk"
@@ -11,6 +22,8 @@ if [ "$1" == "build_and_run" ]; then
         fi
     fi
 
+    # Clone, configure, build and test PcapPlusPlus
+    git clone http://github.com/seladb/PcapPlusPlus.git && \
     cd PcapPlusPlus && \
     ./configure-linux.sh $CONFIG_PARAMS && \
     make all && \
@@ -18,27 +31,28 @@ if [ "$1" == "build_and_run" ]; then
     cd ../Pcap++Test && Bin/Pcap++Test -n $3 && \
     cd ../../ && make install && \
     cd Examples/Tutorials/Tutorial-HelloWorld && make -f Makefile.non_windows all && ./Tutorial-HelloWorld
+
+    # This is the end of the part that runs inside the container
     exit
 fi
 
 IMAGE=$1
 
+# This cleanup function stops the container
 clean_up() {
     docker stop $IMAGE >> /dev/null
-    sudo rm -rf PcapPlusPlus
 }
 
+# Pull the image from DockerHub
 docker pull seladb/$IMAGE
 
-if [ -d "PcapPlusPlus" ] ; then
-    rm -rf PcapPlusPlus
-fi
+# Create a container, mount this script to it
+docker run -itd --rm --name $IMAGE -v $(pwd)/test-pcapplusplus.sh:/test-pcapplusplus.sh seladb/$IMAGE bash
 
-git clone http://github.com/seladb/PcapPlusPlus.git
-
-sudo docker run -itd --rm --name $IMAGE -v $(pwd)/PcapPlusPlus:/PcapPlusPlus -v $(pwd)/test-pcapplusplus.sh:/test-pcapplusplus.sh seladb/$IMAGE bash
+# Whatever happens, do cleanup
 trap clean_up EXIT
 
+# Look if the image has "dpdk" or "pfring" in it - if so special configuration is required
 CONFIG="default"
 if [[ $IMAGE == *"dpdk"* ]]; then
     CONFIG="dpdk"
@@ -48,6 +62,7 @@ else
   fi
 fi
 
-sudo docker exec -i $IMAGE bash -c "/test-pcapplusplus.sh build_and_run $CONFIG $2"
+# Run this script inside the container with a special "build_and_run" argument
+docker exec -i $IMAGE bash -c "/test-pcapplusplus.sh build_and_run $CONFIG $2"
 
 echo "SUCCESS!!"
